f529e499324b249f97b981477be802153de31728
Essia <essia@MacBook-Air-von-Essia.local>
2026-08-09 20:06:59 +0200

    cpp01/ex02/: fixed

--- cpp_MODUL01/ex02/main.cpp
+++ cpp_MODUL01/ex02/main.cpp
@@ -8,7 +8,7 @@ int main()
 	std::string &stringREF = str;
 
 	std::cout << "Address of the string variable: " << &str << std::endl;
-	std::cout << "Address held by stringPTR: "<< &stringPTR << std::endl;
+	std::cout << "Address held by stringPTR: "<< stringPTR << std::endl;
 	std::cout << "Address held by stringREF: " << &stringREF << std::endl << std::endl;
 
 	std::cout << "Value of the string variable: " << str << std::endl;
