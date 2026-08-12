30de366a8ddfcbfb857f54c58ffaa11eb7a4ff91
Essia <essia@MacBook-Air-von-Essia.local>
2026-08-12 17:34:01 +0200

    CPP01/ex03: Implemented the member functions of Weapon

--- cpp_MODUL01/ex03/Weapon.cpp
+++ cpp_MODUL01/ex03/Weapon.cpp
@@ -4,3 +4,15 @@ Weapon::Weapon()
 {
 	type = "";
 }
+Weapon::~Weapon(){};
+
+void Weapon::setType(std::string _type)
+{
+	type = _type;
+}
+
+const std::string &Weapon::getType()
+{
+	return (type);
+}
+
