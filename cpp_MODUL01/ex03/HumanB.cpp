44eb9cf8fa69e8a880ab1686ba15d8f6b85a5485
Essia Komo <eskomo@2-F-9.42heilbronn.de>
2026-08-14 04:37:15 +0200

    CPP01/ex03: Implemented all member function of HumanB in HumanB.cpp

---
+++ cpp_MODUL01/ex03/HumanB.cpp
@@ -0,0 +1,17 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string _name)
+{
+	name = _name;
+}
+
+HumanB::~HumanB(){}
+
+void HumanB::attack()
+{
+	std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+}
+
+void HumanB::setWeapon(Weapon &_weapon) : weapon(_weapon)
+{
+}
