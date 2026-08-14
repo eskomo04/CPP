a3db50ee4d74f8f5dc4a6e81348382d13c07e52f
Essia Komo <eskomo@2-F-9.42heilbronn.de>
2026-08-14 05:11:07 +0200

    CPP/ex03: /HumanB.cpp: fixed the assignment, to assign the memory address of the passed object-reference. Implemented a test main() and also a Makefile

--- cpp_MODUL01/ex03/HumanB.cpp
+++ cpp_MODUL01/ex03/HumanB.cpp
@@ -12,6 +12,7 @@ void HumanB::attack()
 	std::cout << name << " attacks with their " << weapon->getType() << std::endl;
 }
 
-void HumanB::setWeapon(Weapon &_weapon) : weapon(_weapon)
+void HumanB::setWeapon(Weapon &_weapon)
 {
+	weapon = &_weapon;
 }
