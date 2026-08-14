6f8c11295395c21ac0d241cae416fd3bed630cfc
Essia Komo <eskomo@2-F-9.42heilbronn.de>
2026-08-14 04:30:08 +0200

    Cpp01/ex03: Declared HumanB class in HumanB

--- cpp_MODUL01/ex03/HumanA.hpp
+++ cpp_MODUL01/ex03/HumanA.hpp
@@ -1,8 +1,6 @@
 #ifndef HUMANA_HPP
 # define HUMANA_HPP
 
-#include <iostream>
-#include <string>
 #include "Weapon.hpp"
 
 class	HumanA
---
+++ cpp_MODUL01/ex03/HumanB.hpp
@@ -0,0 +1,17 @@
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+#include "Weapon.hpp"
+
+class HumanB{
+private :
+	std::string name;
+	Weapon *weapon;
+public :
+	HumanB(std::string _name);
+	~HumanB();
+	void attack();
+	void setWeapon(Weapon &_weapon);
+};
+
+#endif
